f432b5f213a1d0839836b024b84f35ace1289fbd
Hunter Stevens <hunterstevens1996@gmail.com>
2020-03-11 07:16:04 -0600

    Got Drag event to start but Drop event not working corectly

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ const destinHead = document.querySelectorAll('div.destination h4');
 const aLinks = document.querySelectorAll('a');
 const destinBTN = document.querySelectorAll('.btn');
 const headerSect = document.querySelectorAll('h2');
-const placeForHead = document.querySelectorAll('.intro, .text-content, .content-destination');
+const placeForHead = document.querySelectorAll('.listHead');
 
 let draggedHeader = null;
 
@@ -51,7 +51,44 @@ Array.from(allImg).forEach(function(allImg){
 });
 
 //Drag and Drop Event
+for(let i = 0; i < headerSect.length; i++){
+     const item = headerSect[i];
 
+     item.addEventListener('dragstart', function(){
+         draggedHeader = item;
+         setTimeout(function(){
+             console.log('drag start event');
+             item.style.display = 'none';
+         }, 0);
+     });
+
+     item.addEventListener('dragend', function(){
+         setTimeout(function(){
+             console.log('drag end event');
+             draggedHeader.style.display = 'block';
+             draggedHeader = null;
+         }, 0);
+     });
+
+     for(let j = 0; j < placeForHead.length; j++){
+         const list = placeForHead[j];
+
+        list.addEventListener('dragover', (e) => {
+            console.log('drag over event.');
+            e.preventDefault();
+        });
+
+        list.addEventListener('dragenter', (e) => {
+            console.log('drag enter event');
+            e.preventDefault();
+        })
+
+         list.addEventListener('drop', (e) => {
+             console.log('drop event');
+             list.prepend(draggedHeader);
+         });
+     }
+ }
 
 Array.from(para).forEach(function(para){
     para.addEventListener('mouseover', (e) => {
